08c7a63eb3bf86f6e8c429cee283faae67a30785
Kadeem Wharton <ka.wharton10@gmail.com>
2016-05-24 17:52:09 -0400

    material design setup

---
+++ ProjectX/www/js/app.js
@@ -0,0 +1,80 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+
+angular.module("ionic").provider("$ionicMaterialConfig", function() {
+  var e = this;
+  this.allPlatforms = !1, e.$get = function() {
+    return e
+  }
+}), angular.module("ionic").directive("button", ["$ionicPlatform", function(e) {
+  return {
+    restrict: "E",
+    compile: function(n, t) {
+      t.hasOwnProperty("noRipple") || n.addClass("mdl-js-button mdl-js-ripple-effect"), e.ready(function() {
+        componentHandler.upgradeElement(n[0], "MaterialButton"), componentHandler.upgradeElement(n[0], "MaterialRipple")
+      })
+    }
+  }
+}]), angular.module("ionic").directive("ionTabNav", ["$ionicPlatform", function(e) {
+  return {
+    restrict: "E",
+    compile: function(n, t) {
+      t.hasOwnProperty("noRipple") || n.addClass("mdl-tabs__tab"), e.ready(function() {
+        componentHandler.upgradeElement(n[0], "MaterialButton"), componentHandler.upgradeElement(n[0], "MaterialRipple")
+      })
+    }
+  }
+}]), angular.module("ionic").directive("ionTabs", ["$ionicPlatform", function(e) {
+  return {
+    restrict: "E",
+    compile: function(n, t) {
+      t.hasOwnProperty("noRipple") || n.addClass("mdl-js-tabs mdl-js-ripple-effect"), e.ready(function() {
+        componentHandler.upgradeElement(n[0], "MaterialTabs"), componentHandler.upgradeElement(n[0], "MaterialRipple")
+      })
+    }
+  }
+}]);
+
+
+//*** DO NOT TROUBLE ABOVE CODE!! ***
+
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+  })
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+ $stateProvider.state('groups', {
+   url: '/groups',
+   templateUrl: 'templates/groups.html',
+   //controller: 'home'
+ });
+
+ $urlRouterProvider.otherwise('/groups');
+});
+
+
+
+
+
+
