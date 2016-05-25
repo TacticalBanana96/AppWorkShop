9ebc66d0f13194b198763d8373dd88f635005b95
Kadeem Wharton <ka.wharton10@gmail.com>
2016-05-25 16:40:41 -0400

    navigation from groups to group

--- ProjectX/www/controllers/controllers.js
+++ ProjectX/www/controllers/controllers.js
@@ -1,18 +1,38 @@
 angular.module('starter.controllers', [])
 
-.controller('GroupAdd', function($scope) {
 
-	$scope.random = "kadeem";
+ //Daria's controllers
 
-	
+
+ //Ryvon's controllers
+
+ 
+ //Renaco's controllers
+
+
+ //Kadeem's controllers
+.controller('Group', function($scope, $stateParams) {
+
+	$scope.groupData = "Software Engineering";
+	$scope.randomVAr = $stateParams.group_id;
+	console.log($scope.randomVAr);
+
 })
 
-.controller('Groups', function($scope,$location) {
+//Controller for adding group
+.controller('GroupAdd', function($scope) {
+
+	$scope.random = "kadeem"; //For testing purposes
+
+})
+
+//Controller for groups 
+.controller('Groups', function($scope,$location,Groups) {
+
+	$scope.groups = Groups.all();
+	console.log($scope.groups);
 
 	$scope.gotoURL = function(path){
-
-		console.log("testinggggg");
-		//$urlRouterProvider.otherwise(path);
 		$location.path(path);
 	};
 });
