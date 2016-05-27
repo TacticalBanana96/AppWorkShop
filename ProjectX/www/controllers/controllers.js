c3b664b8425fc6207d790eed24d4808f1f3be9b0
TacBan <daria_gonsalves@hotmail.com>
2016-05-27 07:51:38 -0400

    facebook names now showing on chat

--- ProjectX/www/controllers/controllers.js
+++ ProjectX/www/controllers/controllers.js
@@ -1,35 +1,84 @@
  //Daria's controllers
 angular.module('starter.controllers', ["ionic",'firebase'])
 
-.controller('ChatsCtrl', ['$scope', '$stateParams', '$firebaseArray', '$rootScope', '$ionicHistory', '$state',
-      function($scope, $stateParams, $firebaseArray, $rootScope, $ionicHistory, $state) {
-                $rootScope.notifyIonicGoingBack = function() {
+.controller('ChatsCtrl', ['$scope', '$stateParams', '$firebaseArray', '$rootScope', '$ionicHistory', '$state','FBdata',
+      function($scope, $stateParams, $firebaseArray,$firebaseAuth, $rootScope, $ionicHistory, $state, FBdata) {
+               $rootScope.notifyIonicGoingBack = function() {
                    $state.go('groups');
                 }
                 $scope.$myBack = function() {
                     $ionicHistory.goBack();
                     $rootScope.notifyIonicGoingBack();
                   };
+                 
+                  //$scope.fb_id = ;
                 console.log('state ' + $stateParams);
                 var ref = new Firebase('https://projectxu.firebaseio.com/chats') //USE OUR Firebase FOR CHAT!!
-                $scope.sender_id = 1;
+                
+
+              
+                //$scope.sender_id = $scope.fbdata;
+
+                //$scope.fbname = $scope.fbdata.displayName;
                 $scope.group_id = $stateParams.group_id;
+ 				//$scope.fbname = "BOB";
+ 				//$scope.sender_id = 1;
                 var sync = $firebaseArray(ref);
                 sync.$loaded(function (data) {
-                    var filtered_chats = new Array();
-                    angular.forEach(data, function(value, key) {
-                      if(value.group_id == $scope.group_id) {
-                        filtered_chats.push(value);
-
-                      }
-                    });
-                    $scope.chats = filtered_chats;
+                   // var filtered_chats = new Array();
+                    //angular.forEach(data, function(value, key) {
+                     // if(value.group_id == $scope.group_id) {
+                  //      filtered_chats.push(value);
+                   
+                  //   }
+                   // });
+                    //$scope.chats = filtered_chats;
+                    $scope.chats = data;
+                    
                 });
+               var ref2 = new Firebase('https://projectxu.firebaseio.com/users')
+               var sync2 = $firebaseArray(ref2);
+               console.log(sync2);
+
+               	$scope.user = sync2;
+
+				/*var count = 0;
+					ref2.on("child_added", function(snap) {
+					  count++;
+					  console.log("added", snap.key());
+				});
+				// length will always equal count, since snap.val() will include every child_added event
+				// triggered before this point
+				ref2.once("value", function(snap) {
+				  console.log("initial data loaded!", Object.keys(snap.val()).length === count);
+				});*/
+               	// Retrieve new posts as they are added to our database
+				ref2.on("child_added", function(snapshot, prevChildKey) {
+				    var newPost = snapshot.val();
+	  				console.log("Name: " + newPost.displayName);
+	  				console.log("id: " + newPost.id);
+				});
+
+				ref2.on("child_changed", function(snapshot) {
+					 var changedPost = snapshot.val();
+					 console.log("!!!!!Name: " + newPost.displayName);
+	  				console.log("!!!!!!id: " + newPost.id);
+				});
+				// retrieve the last record from `ref`
+				ref2.endAt().limitToLast(1).on('child_added', function(snapshot) {
 
 
+				   // all records after the last continue to invoke this function
+				   console.log("Lastname" + snapshot.key(), snapshot.val().displayName);
+				   $scope.sender_name = snapshot.val().displayName;
+				   $scope.sender_id = snapshot.val().id;
+
+
+				});
 
 
                 $scope.sendChat = function(chat){
+                	
 
                   //if($rootScope.authData){
                   /*  $scope.chats.$add({
@@ -37,26 +86,36 @@ angular.module('starter.controllers', ["ionic",'firebase'])
                       message: chat.message,
                       imgURL: $rootScope.authData.facebook.cachedUserProfile.profile_img_url
                     });
+
                   */
+                  //console.log($rootScope.authData);
 
                   $scope.chats.$add({
-                      user_id: $scope.sender_id, //SWITCH TO FACEBOOK!!!
+                  	 
+                      sender_id: $scope.sender_id, 
+                      sender_name: $scope.sender_name,//SWITCH TO FACEBOOK!!!
                       message: chat.message, 
                       group_id: $scope.group_id, 
-                      timestamp: new Date().getTime()
+                      timestamp: new Date().getTime(),
+                      //sender_name: $rootScope.authData.facebook.cachedUserProfile.displayName,
+                      //imgURL: $rootScope.authData.facebook.cachedUserProfile.profile_img_url
                     });
                     chat.message ="";
-                  //}
+                  
 
                 }
+           // }
+
+
+
 
 
 }])
 
 
  //Ryvon's controllers
-.controller('DashCtrl', function($scope, $state, $firebaseAuth, $firebaseArray, $filter,FBdata) {
-console.log('aaaaa');
+.controller('DashCtrl', function($scope, $state, $firebaseAuth, $firebaseArray, $filter,FBdata, $rootScope) {
+//console.log('aaaaa');
 $scope.fbdata = FBdata.all();
 console.log($scope.fbdata);
 
@@ -78,7 +137,9 @@ $scope.login = function(){
             })
           }
             $scope.fbdata.$add(authData.facebook);
+            $rootScope.fbdata2 = authData;
       }
+    
 
     },{scope:"public_profile,email"});
 
